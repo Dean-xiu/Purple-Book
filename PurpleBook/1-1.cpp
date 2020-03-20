a0647de6a2cc734719843258b5a4a4e59395990a
Dean-xiiu <alien_B612@163.com>
2020-03-20 10:36:50 +0800

    first commit

---
+++ PurpleBook/1-1.cpp
@@ -0,0 +1,22 @@
+// 算术表达式
+#include<stdio.h>
+
+int main()
+{
+	printf_s("test0\n");
+	printf_s("%d\n", 1 + 2);
+
+	printf_s("test1\n");
+	printf_s("%d\n", 3-4);
+
+	printf_s("test2\n");
+	printf_s("%d\n", 5*6);
+
+	printf_s("test3\n");
+	printf_s("%d\n", 8/4);
+
+	printf_s("test4\n");
+	printf_s("%d\n", 8/5);
+
+	
+}
